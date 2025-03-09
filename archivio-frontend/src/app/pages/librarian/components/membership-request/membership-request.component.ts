963562a7a1abad332348131f3b65aff9d748fa80
Anirban Basak <jeetbasak2002@gmail.com>
2025-03-09 10:36:39 +0530

    feat : Configured the internal routing of the librarian module.

---
+++ archivio-frontend/src/app/pages/librarian/components/membership-request/membership-request.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-membership-request',
+  templateUrl: './membership-request.component.html',
+  styleUrls: ['./membership-request.component.css'],
+})
+export class MembershipRequestComponent {}
--- archivio-frontend/src/app/pages/librarian/librarian.module.ts
+++ archivio-frontend/src/app/pages/librarian/librarian.module.ts
@@ -2,12 +2,35 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LibrarianComponent } from './librarian.component';
 import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { BookAddComponent } from './components/book-add/book-add.component';
+import { BookRequestComponent } from './components/book-request/book-request.component';
+import { MembershipRequestComponent } from './components/membership-request/membership-request.component';
 
 // These are the routes for this module
-const route: Routes = [{ path: '', component: LibrarianComponent }];
+const route: Routes = [
+  {
+    path: '',
+    component: LibrarianComponent,
+    children: [
+      { path: '', redirectTo: 'book-list', pathMatch: 'full' },
+      { path: 'book-list', component: BookListComponent },
+      { path: 'book-add', component: BookAddComponent },
+      { path: 'book-request', component: BookRequestComponent },
+      { path: 'membership-request', component: MembershipRequestComponent },
+    ],
+  },
+];
 
 @NgModule({
-  declarations: [LibrarianComponent],
-  imports: [CommonModule, RouterModule.forChild(route)],
+  declarations: [
+    LibrarianComponent,
+    BookListComponent,
+    BookAddComponent,
+    BookRequestComponent,
+    MembershipRequestComponent,
+  ],
+  imports: [CommonModule, RouterModule.forChild(route), SharedModule],
 })
 export class LibrarianModule {}
