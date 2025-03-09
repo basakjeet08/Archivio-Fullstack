336030d96a841bd40ab39b6dba91fcd2e0bece79
Anirban Basak <jeetbasak2002@gmail.com>
2025-03-09 10:30:50 +0530

    feat : Configured the internal routing of the Admin Module.

--- archivio-frontend/src/app/pages/admin/admin.module.ts
+++ archivio-frontend/src/app/pages/admin/admin.module.ts
@@ -2,12 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin.component';
 import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { LibrarianListComponent } from './components/librarian-list/librarian-list.component';
+import { LibrarianAddComponent } from './components/librarian-add/librarian-add.component';
 
 // These are the routes for this module
-const route: Routes = [{ path: '', component: AdminComponent }];
+const route: Routes = [
+  {
+    path: '',
+    component: AdminComponent,
+    children: [
+      { path: '', redirectTo: 'librarian-list', pathMatch: 'full' },
+      { path: 'librarian-list', component: LibrarianListComponent },
+      { path: 'librarian-add', component: LibrarianAddComponent },
+    ],
+  },
+];
 
 @NgModule({
-  declarations: [AdminComponent],
-  imports: [CommonModule, RouterModule.forChild(route)],
+  declarations: [AdminComponent, LibrarianListComponent, LibrarianAddComponent],
+  imports: [CommonModule, RouterModule.forChild(route), SharedModule],
 })
 export class AdminModule {}
---
+++ archivio-frontend/src/app/pages/admin/components/librarian-list/librarian-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-librarian-list',
+  templateUrl: './librarian-list.component.html',
+  styleUrls: ['./librarian-list.component.css'],
+})
+export class LibrarianListComponent {}
