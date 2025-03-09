02fe339ee1b1a8d86ba107bb0b2d85d0a5fd5c99
Anirban Basak <jeetbasak2002@gmail.com>
2025-03-09 10:14:00 +0530

    feat : Configured and generated templates for all the modules needed.

---
+++ archivio-frontend/src/app/pages/librarian/librarian.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LibrarianComponent } from './librarian.component';
+
+@NgModule({
+  declarations: [LibrarianComponent],
+  imports: [CommonModule],
+})
+export class LibrarianModule {}
