146879d5f4fab22e080fcb734be7015b17cf7fe2
Anirban Basak <jeetbasak2002@gmail.com>
2025-03-09 09:44:17 +0530

    initial Backend Commit

---
+++ archivio-backend/src/main/java/dev/anirban/archivio_backend/ArchivioBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.anirban.archivio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchivioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchivioBackendApplication.class, args);
+	}
+
+}
