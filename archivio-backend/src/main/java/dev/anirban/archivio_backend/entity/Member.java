2f4f4cef17fa558e626b619185846286cc694a92
Anirban Basak <jeetbasak2002@gmail.com>
2025-03-15 19:22:20 +0530

    refactor : Created a base User class which is extended by the Admin, Librarian and Member Classes for Inheritance based Entity Classes.

--- archivio-backend/src/main/java/dev/anirban/archivio_backend/entity/Member.java
+++ archivio-backend/src/main/java/dev/anirban/archivio_backend/entity/Member.java
@@ -1,64 +1,14 @@
 package dev.anirban.archivio_backend.entity;
 
 
-import dev.anirban.archivio_backend.dto.response.UserDto;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.*;
-import org.hibernate.annotations.UuidGenerator;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
-import java.util.Collection;
-import java.util.List;
-
-@Getter
-@Setter
-@ToString
-@Builder
 @NoArgsConstructor
-@AllArgsConstructor
+@SuperBuilder
 @Entity
 @Table(name = "Member_DB")
-public class Member implements UserDetails {
-
-    @Id
-    @UuidGenerator
-    @Column(name = "id")
-    private String id;
-
-    @Column(name = "name", nullable = false)
-    private String name;
-
-    @Column(name = "email", nullable = false, unique = true)
-    private String email;
-
-    @Column(name = "password", nullable = false)
-    private String password;
-
-    @Column(name = "role", nullable = false)
-    private Role role;
-
-    public UserDto toUserDto() {
-        return UserDto
-                .builder()
-                .id(id)
-                .name(name)
-                .email(email)
-                .role(role.toString())
-                .build();
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.toString()));
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
+public class Member extends User {
 }
---
+++ archivio-backend/src/main/java/dev/anirban/archivio_backend/entity/User.java
@@ -0,0 +1,62 @@
+package dev.anirban.archivio_backend.entity;
+
+import dev.anirban.archivio_backend.dto.response.UserDto;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.UuidGenerator;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+public abstract class User implements UserDetails {
+
+    @Id
+    @UuidGenerator
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    public UserDto toUserDto() {
+        return UserDto
+                .builder()
+                .id(id)
+                .name(name)
+                .email(email)
+                .role(role.toString())
+                .build();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.toString()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
