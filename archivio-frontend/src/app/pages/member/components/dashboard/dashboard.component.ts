ad093d539b89c2bcc6bacccc7891086af524ee65
Anirban Basak <jeetbasak2002@gmail.com>
2025-03-16 01:52:57 +0530

    feat : Implemented the UI Layer for the Member Dashboard after API Calls.

--- archivio-frontend/src/app/pages/member/components/dashboard/dashboard.component.ts
+++ archivio-frontend/src/app/pages/member/components/dashboard/dashboard.component.ts
@@ -1,14 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { StatsDao } from 'src/app/shared/Models/stats/StatsDao';
+import { StatsService } from 'src/app/shared/services/stats.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  // This is the stats data for the component
+  statsData: StatsDao | null = null;
+
+  // loading and Error states
+  isLoading: boolean = false;
+  errorMessage: string | null = null;
+
   // Injecting all the necessary dependencies
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private statsService: StatsService
+  ) {}
+
+  // Calling the api calls in the start and subscribing them
+  ngOnInit(): void {
+    // Setting the loading state
+    this.isLoading = true;
+
+    this.statsService.fetchMemberStats().subscribe({
+      // Success State
+      next: (data: StatsDao) => {
+        this.isLoading = false;
+        this.statsData = data;
+      },
+
+      // Error State
+      error: (error: Error) => {
+        this.isLoading = false;
+        this.errorMessage = error.message;
+      },
+    });
+  }
 
   // This function is invoked when the user clicks the book list component
   onBookListClick() {
@@ -25,5 +58,10 @@ export class DashboardComponent {
   // This function is invoked when the user clicks on the return books component
   onReturnBooksClick() {
     this.router.navigate(['../', 'to-be-returned'], { relativeTo: this.route });
+  }
+
+  // This funciton is invoked when the error cancel button is clicked
+  onErrorCancelClick() {
+    this.errorMessage = null;
   }
 }
